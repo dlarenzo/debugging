3c5f0b0814c759290640a1234f7b615fe403cd23
LaRenzo <dlarenzo@outlook.com>
2024-01-14 15:44:06 -0700

    html and css fixed

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 // make sure these match the HTML element names for the ID then class.
-let input = document.querySelector("#NAME");
-let display = document.querySelector(".displaymycode");
+let input = document.querySelector("#name");
+let display = document.querySelector(".display");
 
 // This code should be working
 input.addEventListener("input", function () {
-//   const name = input.value;
-//   display.textContent = name;
-// });
+const name = input.value;
+display.textContent = name;
+});
 
 // missing function?
 
